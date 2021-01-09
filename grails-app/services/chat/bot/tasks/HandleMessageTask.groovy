b53b2587a39a3bd22d0e4a8b3457e4cbea9132af
Robert Biały <bialyrobert12@gmail.com>
2021-01-09 14:34:39 +0100

    Deployment setup

--- grails-app/services/chat/bot/tasks/HandleMessageTask.groovy
+++ grails-app/services/chat/bot/tasks/HandleMessageTask.groovy
@@ -18,7 +18,7 @@ class HandleMessageTask implements Runnable {
 
     @Override
     void run() {
-        Collection<DailyTrend> dailyTrends = googleTrendsManager.getDailyTrends()
+        //Collection<DailyTrend> dailyTrends = googleTrendsManager.getDailyTrends()
         messengerService.SendMessage(requestMessage.sender.id, "Test message")
     }
 }
